48afa93fa93af0e2610df6f44e012a56f01d2ad0
NDRW21 <andrewamadorg1@gmail.com>
2026-03-08 13:21:52 -0600

    Clase-6

---
+++ Clase-6/main.js
@@ -0,0 +1,19 @@
+console.log("Hola!");
+
+const age = 22;
+var number = 56;
+
+let numberFloat = 2.5;
+console.log(numberFloat + 18);
+
+let numberWithZero = 15.6;
+console.log(numberWithZero + 18);
+
+/* Booleanos */
+
+let FALSE
+
+let TRUE
+
+/* Operadores */
+console.log("")
---
+++ Clase-6/otro.js
@@ -0,0 +1,15 @@
+let lun = "lunes"
+let mar = "martes"
+let mie = "miercoles"
+let jue = "jueves"
+let vie = "viernes"
+
+let hoy = mie
+
+console.log("Hoy es lunes?", hoy == lun)
+console.log("Hoy es miercoles?", hoy == mie)
+console.log("Hoy es martes o miercoles?", mar || mie == hoy)
+console.log("Hoy es miercoles", hoy == mie)
+
+
+
